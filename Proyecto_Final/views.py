0f98c856bf554620214e3a925c5e2b86941d2716
Mailen Gomez <maichu19.mg@gmail.com>
2024-07-11 23:38:46 -0300

    Creo los template html de las vistas

--- Proyecto_Final/views.py
+++ Proyecto_Final/views.py
@@ -1,16 +1,67 @@
 from django.http import HttpResponse
+from django.template import Template, Context
 
 def inicio(request):
-    return HttpResponse("Inicio")
+    inicio_html = open('./Proyecto_Final/plantillas/index.html')
+
+    plantilla = Template(inicio_html.read())
+
+    inicio_html.close()
+
+    mi_contexto = Context()
+
+    documento = plantilla.render(mi_contexto)
+
+    return HttpResponse(documento)
 
 def login(request):
-    return HttpResponse("Login")
+    login_html = open('./Proyecto_Final/plantillas/login.html')
+
+    plantilla = Template(login_html.read())
+
+    login_html.close()
+
+    mi_contexto = Context()
+
+    documento = plantilla.render(mi_contexto)
+
+    return HttpResponse(documento)
 
 def registro(request):
-    return HttpResponse("Registro")
+    registro_html = open('./Proyecto_Final/plantillas/registro.html')
+
+    plantilla = Template(registro_html.read())
+
+    registro_html.close()
+
+    mi_contexto = Context()
+
+    documento = plantilla.render(mi_contexto)
+
+    return HttpResponse(documento)
 
 def detalle(request, id_producto):
-    return HttpResponse(f"Detalle producto ID: {id_producto}")
+    detalle_html = open('./Proyecto_Final/plantillas/detalle.html')
+
+    plantilla = Template(detalle_html.read())
+
+    detalle_html.close()
+
+    mi_contexto = Context()
+
+    documento = plantilla.render(mi_contexto)
+
+    return HttpResponse(documento)
 
 def perfil(request, id_usuario):
-    return HttpResponse(f"Perfil usuario ID: {id_usuario}")
+    perfil_html = open('./Proyecto_Final/plantillas/perfil.html')
+
+    plantilla = Template(perfil_html.read())
+
+    perfil_html.close()
+
+    mi_contexto = Context()
+
+    documento = plantilla.render(mi_contexto)
+
+    return HttpResponse(documento)
